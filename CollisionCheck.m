bdbf59f334d31b85d2c96a0319f95751cca2f970
rachannbanks <banksr@sas.upenn.edu>
2016-05-07 10:47:13 -0400

    Added files via upload
    new version of collision check, still has old version commented out

---
+++ CollisionCheck.m
@@ -0,0 +1,113 @@
+function collision = CollisionCheck(boardsz, snake, newX, newY, dot)
+%this function checks for any of the following three instances
+%1-if the snake hits a wall
+%2-if the snake hits itself
+%3-if the snake eats a dot
+%collision returns zero if doesn't hit wall or self, one otherwise
+%needs to get called after plotsnake
+currentX=snake.XData;
+currentY=snake.YData;
+collision=0;
+nlinks=length(newX)-1; %current size of snake
+%round dot position to whole numbers
+dotx=dot.XData;
+doty=dot.YData;
+roundX=round(dotx);
+roundY=round(doty);
+%1-check if snake hits a wall
+if any(newX == -boardsz) || any(newX == boardsz)
+    disp('end game');
+    collision=1;
+    return;
+elseif any(newY == -boardsz) || any(newY == boardsz)
+    disp('end game');
+    collision=1;
+    return;
+end
+%2-check if snake hits itself-only need to check first link
+firstX=newX(1);
+firstY=newY(1);
+%check if any of the links have the same coordinates as the new first link
+if nlinks > 1
+    for i=2:nlinks
+        if newX(i) == firstX
+            if newY(i) == firstY
+                collision=1;
+            end
+        end
+    end
+end
+%3-check if snake eats dot
+%initialize eat variable
+eat=0; %will be changed to one if snake eats
+%check if first link eats the dot
+%if moving horizontally-if passes dot and dot is between y coordinates
+if currentX(1) < dotx && firstX > dotx
+    if (firstY-0.5) <= doty && (firstY+0.5)>= doty
+        eat=1;
+    end
+elseif currentX(1) > dotx && firstX < dotx
+    if (firstY-0.5) <= doty && (firstY+0.5)>= doty
+        eat=1;
+    end
+    %if moving vertically-if passes and dot is between x coordinates
+elseif currentY(1) < doty && firstY > doty
+    if (firstX-0.5)<= dotx && (firstX+0.5)>= dotx
+        eat = 1;
+    end
+elseif currentY(1) > doty && firstY < doty
+    if (firstX-0.5)<= dotx && (firstX+0.5)>= dotx
+        eat = 1;
+    end
+end
+% for i=1:nlinks
+%     if currentX(i) < dotx && newX(i) > dotx
+%         if currentY(i) == roundY
+%             disp('eat');
+%             eat= 1;
+%             %UpdateDot();
+%             break;
+%         end
+% 
+%     elseif currentX(i) > dotx && newX(i) < dotx
+%         if currentY(i) == roundY
+%             disp('eat');
+%             eat=1;
+%             %UpdateDot();                  
+%             break;
+%         end
+% 
+%     elseif currentY(i) < doty && newY(i) > doty
+%         if currentX(i) == roundX
+%             disp('eat');
+%             eat=1;
+%             %UpdateDot();
+%             break;
+%         end
+% 
+%     elseif currentY(i) > doty && newY(i) < doty
+%         if currentX(i) == roundX
+%             disp('eat');
+%             eat=1;
+%             %UpdateDot();
+%             break;
+%         end
+% 
+%     end
+% end
+%if eat variable is unchanged shorten the snake and plot it
+if eat == 0;
+    %make position arrays the same length as the number of links
+    newX=newX(1:nlinks);
+    newY=newY(1:nlinks);
+    %update snake plot if snake does not eat
+    snake.XData=newX;
+    snake.YData=newY;
+elseif eat == 1
+    %if dot is eaten create new dot
+    UpdateDot(boardsz, dot);
+    %lengthen snake
+    snake.XData=newX;
+    snake.YData=newY;
+end
+
