160f4649d4bf1ac549c80ba85b245b301aa243ba
rachannbanks <banksr@sas.upenn.edu>
2016-05-07 11:22:15 -0400

    new Collision check again
    see if first coordinate is the same as dot's coordinates

--- CollisionCheck.m
+++ CollisionCheck.m
@@ -42,22 +42,10 @@ end
 eat=0; %will be changed to one if snake eats
 %check if first link eats the dot
 %if moving horizontally-if passes dot and dot is between y coordinates
-if currentX(1) < dotx && firstX > dotx
-    if (firstY-0.6) <= doty && (firstY+0.6)>= doty
+%new version-check if coordinates equal the dot coordinates
+if (firstX-0.6)<=dotx && (firstX+0.6)>= dotx
+    if (firstY-0.6) <= doty && (firstY+0.6) >= doty
         eat=1;
-    end
-elseif currentX(1) > dotx && firstX < dotx
-    if (firstY-0.6) <= doty && (firstY+0.6)>= doty
-        eat=1;
-    end
-    %if moving vertically-if passes and dot is between x coordinates
-elseif currentY(1) < doty && firstY > doty
-    if (firstX-0.6)<= dotx && (firstX+0.6)>= dotx
-        eat = 1;
-    end
-elseif currentY(1) > doty && firstY < doty
-    if (firstX-0.6)<= dotx && (firstX+0.6)>= dotx
-        eat = 1;
     end
 end
 
